01b1165495e9616e14263b97508bdeb632eda51a
Dan Robertson <dan.robertson@anidata.org>
2017-09-03 04:57:06 +0000

    Use rasp and fix data handler
     - Use rasp for http goodness
     - Punt the old data handler
       - Create a data handler thread that bulk inserts at some interval
       - The worker threads now just post their retrieved pages to this
         list of pages to be bulk inserted
       - Remove MongoDB stuff
       - Remove wierd exceptions
     - Remove old codez
     - Re-License to a more liberal license (BSD)
    
    This is far from complete, but this removes a lot of old stuff that
    either doesn't work or is super fragile. After this 60% of the time it
    *should* work all of the time.

--- palantiri/core/common.py
+++ palantiri/core/common.py
@@ -1,22 +1,86 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# Copyright (c) 2017 Anidata
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
+import threading
 from . import errors
 
-class Website(object):
-    def __init__(self, url = None, source = None):
-        self.url = url
-        self.source = source
+# Technically I've read that many operations are thread-safe on Python's
+# list implementation, so this may not be necessary, but I think I'd rather
+# err on the side of caution at least for now
+class SharedList(object):
+    def __init__(self, lst):
+        self.mutex = threading.Lock()
+        self.lst = lst
         return
 
-    def set_source(self, source):
-        self.source = source
-        return
+    def __contains__(self, val):
+        return val in self.lst
 
-    def set_url(self, url):
-        self.url = url
-        return
+    def __iter__(self):
+        return self.lst.__iter__()
 
-    def __repr__(self):
-        return "url: %s" % self.url
+    def pop(self):
+        self.mutex.acquire()
+        try:
+            val = self.lst.pop()
+            self.mutex.release()
+            return val
+        except:
+            if self.mutex.locked():
+                self.mutex.release()
+            return None
+
+    def append(self, val):
+        self.mutex.acquire()
+        try:
+            self.lst.append(val)
+            self.mutex.release()
+            return True
+        except:
+            if self.mutex.locked():
+                self.mutex.release()
+            return None
+
+    def __len__(self):
+        return len(self.lst)
+
+    def extend(self, lst):
+        self.mutex.acquire()
+        try:
+            self.lst.extend(lst)
+            self.mutex.release()
+            return True
+        except:
+            if self.mutex.locked():
+                self.mutex.release()
+            return True
+
+    def drain(self):
+        self.mutex.acquire()
+        try:
+            local_cpy = self.lst[:]
+            self.lst.clear()
+            self.mutex.release()
+            return local_cpy
+        except:
+            if self.mutex.locked():
+                self.mutex.release()
+            return None
+
